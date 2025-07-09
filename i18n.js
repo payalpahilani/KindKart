6dffbbabab348e9cf7468eab6fbd3da4bd445a89
Akshat <akshatovalekar05@gmail.com>
2025-07-09 12:46:16 -0400

    Updated packages

--- i18n.js
+++ i18n.js
@@ -3,35 +3,21 @@ import { initReactI18next } from 'react-i18next';
 import * as Localization from 'expo-localization';
 import en from './locales/en.json';
 import fr from './locales/fr.json';
-
-const languageDetector = {
-  type: 'languageDetector',
-  async: true,
-  detect: (callback) => {
-    const deviceLang = Localization.locale.slice(0, 2).toLowerCase();
-    if (deviceLang === 'fr') {
-      callback('fr');
-    } else {
-      callback('en'); 
-    }
-  },
-  init: () => {},
-  cacheUserLanguage: () => {}
-};
+import RNLanguageDetector from "@os-team/i18next-react-native-language-detector";
 
 i18n
-  .use(languageDetector)
+  .use(RNLanguageDetector)
   .use(initReactI18next)
   .init({
-    compatibilityJSON: 'v3',
-    fallbackLng: 'en',
+    compatibilityJSON: "v3",
+    fallbackLng: "en",
     resources: {
       en: { translation: en },
-      fr: { translation: fr }
+      fr: { translation: fr },
     },
     interpolation: {
       escapeValue: false,
-    }
+    },
   });
 
 export default i18n;
