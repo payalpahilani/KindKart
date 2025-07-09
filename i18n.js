68375c2c3ddff03c76d372f50219bb7847a3565f
Akshat <akshatovalekar05@gmail.com>
2025-07-09 13:21:26 -0400

    changes for language

--- i18n.js
+++ i18n.js
@@ -1,16 +1,15 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import * as Localization from 'expo-localization';
+import * as Localization from "expo-localization";
 import en from './locales/en.json';
 import fr from './locales/fr.json';
-import RNLanguageDetector from "@os-team/i18next-react-native-language-detector";
 
 i18n
-  .use(RNLanguageDetector)
   .use(initReactI18next)
   .init({
     compatibilityJSON: "v3",
     fallbackLng: "en",
+    lng: Localization.locale.slice(0, 2),
     resources: {
       en: { translation: en },
       fr: { translation: fr },
